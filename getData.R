4de091bc0f11e855167af94f5a9a14dfe379ccf6
Chanyub.Park <mrchypark@gmail.com>
2017-09-28 00:50:15 +0900

    run set

--- getData.R
+++ getData.R
@@ -1,71 +1,39 @@
 library(rvest)
 library(data.table)
-library(R.utils)
 
-options(stringsAsFactors = F)
-
-success <- function(res){
-  cat("Request done! Status:", res$status, "\n")
-  res$content<-iconv(rawToChar(res$content),from="CP949",to="UTF-8")
-  dat <<- c(dat, list(res))
-}
-failure <- function(msg){
-  cat("Oh noes! Request failed!", msg, "\n")
-}
-
-num<-1:5192
-
-
-out<-c()
-getHtml<-function(x){
-  tryget<-try(evalWithTimeout({
-    read_html(x)
-  }, timeout=15, onTimeout="error"), silent = T)
-  n<-1
-  while(class(tryget)[1]!="xml_document"){
-    tryget<-try(evalWithTimeout({
-      read_html(x)
-    }, timeout=15, onTimeout="error"), silent = T)
-    n<-n+1
-    if(n<5){
-      out<<-x
-    }
-  }
-  if(class(tryget)[1]=="xml_document"){
-    return(tryget)
-  }
-}
-
-getCon<-function(x){
-  tit <-
-    x %>% 
-    html_nodes("span.word_title font") %>% 
-    html_text
-  
-  defi <-
-    x %>% 
-    html_nodes("span.Definition") %>% 
-    html_text
-  
-  tem<-data.table(word=tit, definition=defi)
-  return(tem)
-}
-
-
+num<-1:519120
+dat<-c()
+str<-Sys.time()
 for (i in num){
-  k <- (i-1)*100+1
-  tars<-paste0("http://stdweb2.korean.go.kr/search/View.jsp?idx=",k:(k+99),"&go=1")
+  tar<-paste0("http://stdweb2.korean.go.kr/search/View.jsp?idx=",i,"&go=1")
   
-  cont<-lapply(tars, getHtml)
-  cont<-lapply(cont, getCon)
-  cont<-do.call(rbind, cont)
+  root <- tar %>%   
+    read_html 
   
-  dir.create("./data",showWarnings = F)
-  fwrite(
-    cont
-    , paste0("./data/w",i,".csv")
-    , row.names = F
+  (
+    word <-
+      root %>% 
+      html_nodes("span.word_title font") %>% 
+      html_text
   )
-  # dat<-rbind(dat,tem)
-  cat(i, " / 5192\n")
+  (
+    defi <-
+      root %>% 
+      html_nodes("span.Definition") %>% 
+      html_text
+  )
+  
+  tem<-data.table(word=word,definition=defi)
+  dat<-rbind(dat,tem)
+  if(i%%300==0){
+    fwrite(
+      dat
+      , paste0("./data/w",i,".csv")
+      , row.names = F
+    )
+    dat<-c()
+    mid<-Sys.time()
+    cat("save", mid-str,"\n")
+  }
+  cat(i, " / 519120\n")
 }
